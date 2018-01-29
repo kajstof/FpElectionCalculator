ad55eb8ec8f84bb09f6590123313b0a74a7628a3
Krzysztof Krysiak <krysiak86@gmail.com>
2018-01-29 21:55:43 +0100

    Add Pesel Validator Tests

---
+++ FpElectionCalculator.Library/PeselValidator.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FpElectionCalculator.Library
+{
+    // -----------------------------------------------------------------
+    // Class written by Tomasz Lubinski and ported from Java to C#
+    // http://www.algorytm.org/numery-identyfikacyjne/pesel/pesel-j.html
+    // -----------------------------------------------------------------
+    // PS. http://www.bogus.ovh.org/generatory/all.html - is not working
+    // -----------------------------------------------------------------
+    public class PeselValidator
+    {
+
+        private byte[] pesel = new byte[11];
+        private bool valid = false;
+
+        public PeselValidator(string pesel)
+        {
+            if (pesel.Length != 11)
+            {
+                valid = false;
+            }
+            else
+            {
+                for (int i = 0; i < 11; i++)
+                {
+                    this.pesel[i] = byte.Parse(pesel[i].ToString());
+                }
+
+                if (checkMonth() && checkDay() && checkSum())
+                {
+                    valid = true;
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+        }
+
+        public bool IsValid()
+        {
+            return valid;
+        }
+
+        public int getBirthYear()
+        {
+            int year;
+            int month;
+            year = 10 * pesel[0];
+            year += pesel[1];
+            month = 10 * pesel[2];
+            month += pesel[3];
+            if (month > 80 && month < 93)
+            {
+                year += 1800;
+            }
+            else if (month > 0 && month < 13)
+            {
+                year += 1900;
+            }
+            else if (month > 20 && month < 33)
+            {
+                year += 2000;
+            }
+            else if (month > 40 && month < 53)
+            {
+                year += 2100;
+            }
+            else if (month > 60 && month < 73)
+            {
+                year += 2200;
+            }
+            return year;
+        }
+
+        public int getBirthMonth()
+        {
+            int month;
+            month = 10 * pesel[2];
+            month += pesel[3];
+            if (month > 80 && month < 93)
+            {
+                month -= 80;
+            }
+            else if (month > 20 && month < 33)
+            {
+                month -= 20;
+            }
+            else if (month > 40 && month < 53)
+            {
+                month -= 40;
+            }
+            else if (month > 60 && month < 73)
+            {
+                month -= 60;
+            }
+            return month;
+        }
+
+
+        public int getBirthDay()
+        {
+            int day;
+            day = 10 * pesel[4];
+            day += pesel[5];
+            return day;
+        }
+
+        public String getSex()
+        {
+            if (valid)
+            {
+                if (pesel[9] % 2 == 1)
+                {
+                    return "Male";
+                }
+                else
+                {
+                    return "Female";
+                }
+            }
+            else
+            {
+                return "---";
+            }
+        }
+
+        private bool checkSum()
+        {
+            int sum = 1 * pesel[0] +
+            3 * pesel[1] +
+            7 * pesel[2] +
+            9 * pesel[3] +
+            1 * pesel[4] +
+            3 * pesel[5] +
+            7 * pesel[6] +
+            9 * pesel[7] +
+            1 * pesel[8] +
+            3 * pesel[9];
+            sum %= 10;
+            sum = 10 - sum;
+            sum %= 10;
+
+            if (sum == pesel[10])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool checkMonth()
+        {
+            int month = getBirthMonth();
+            int day = getBirthDay();
+            if (month > 0 && month < 13)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool checkDay()
+        {
+            int year = getBirthYear();
+            int month = getBirthMonth();
+            int day = getBirthDay();
+            if ((day > 0 && day < 32) &&
+            (month == 1 || month == 3 || month == 5 ||
+            month == 7 || month == 8 || month == 10 ||
+            month == 12))
+            {
+                return true;
+            }
+            else if ((day > 0 && day < 31) &&
+            (month == 4 || month == 6 || month == 9 ||
+            month == 11))
+            {
+                return true;
+            }
+            else if ((day > 0 && day < 30 && leapYear(year)) ||
+            (day > 0 && day < 29 && !leapYear(year)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool leapYear(int year)
+        {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ FpElectionCalculator.Tests/PeselValidationTests.cs
@@ -0,0 +1,27 @@
+﻿using FpElectionCalculator.Library;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace FpElectionCalculator.Tests
+{
+    public class PeselValidationTests
+    {
+        [Theory]
+        [InlineData(99010112342)]
+        public void ValidPeselReturnsTrue(ulong pesel)
+        {
+            Assert.True(new PeselValidator(pesel.ToString()).IsValid());
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(12121278901234)]
+        [InlineData(12340178901)]
+        [InlineData(67109945623)]
+        public void InvalidPeselReturnsFalse(ulong pesel)
+        {
+            Assert.False(new PeselValidator(pesel.ToString()).IsValid());
+        }
+    }
+}
