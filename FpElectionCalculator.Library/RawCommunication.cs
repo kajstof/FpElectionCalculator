8c3c01f547327aa6b134221cc2bed0d05ddc9839
Krzysztof Krysiak <krysiak86@gmail.com>
2018-01-29 18:18:03 +0100

    Add raw communication class

---
+++ FpElectionCalculator.Library/RawCommunication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+
+namespace FpElectionCalculator
+{
+    public class RawCommunication
+    {
+        private static string getHttp(string queryString)
+        {
+            string url = $"http://webtask.future-processing.com:8069/{queryString}";
+            return new HttpClient().GetAsync(url).Result.Content.ReadAsStringAsync().Result;
+        }
+
+        public string getCandidates()
+        {
+            return getHttp("candidates");
+        }
+
+        public string getPeopleDisallowedToVote()
+        {
+            return getHttp("blocked");
+        }
+    }
+}
